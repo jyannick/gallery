a7f62141a7b6bdccc68837ea25d22474b7cfd9fe
Nnexxus <Nnexxus@gmail.com>
2018-10-02 22:17:44 +0200

    big refactoring, one gallery per directory (prepares for #3)

--- gallery.py
+++ gallery.py
@@ -1,7 +1,26 @@
 import os
+from glob import glob
 from sys import argv
 
 import jinja2
+
+
+def main(directory):
+    generate_galleries_recursively(directory)
+    print("done")
+
+
+def generate_galleries_recursively(dir_in):
+    generate_gallery(dir_in)
+    for root, dirs, files in os.walk(dir_in):
+        for directory in dirs:
+            generate_gallery(os.path.join(root, directory))
+
+
+def generate_gallery(directory):
+    print(f"generating gallery for {directory}")
+    with open(os.path.join(directory, "gallery.html"), "w") as f:
+        f.write(render_gallery(find_images(directory, directory), subdirectories=list_subdirectories(directory)))
 
 
 def find_images(directory, dir_out):
@@ -12,18 +31,16 @@ def find_images(directory, dir_out):
             if file.endswith((".gif", ".bmp", ".jpg"))]
 
 
-def render_template(images):
+def list_subdirectories(directory):
+    return [os.path.relpath(subdir, directory) for subdir in glob(directory + "/*/")]
+
+
+def render_gallery(images, subdirectories):
     template_loader = jinja2.FileSystemLoader(searchpath="./")
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template("template.html")
-    return template.render(images=images)
-
-
-def main(dir_in, dir_out):
-    with open(os.path.join(dir_out, "gallery.html"), "w") as f:
-        f.write(render_template(find_images(dir_in, dir_out)))
-    print("done")
+    return template.render(images=images, subdirectories=subdirectories)
 
 
 if __name__ == "__main__":
-    main(dir_in=argv[1], dir_out=argv[1])
+    main(directory=argv[1])
